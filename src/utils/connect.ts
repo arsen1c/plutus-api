d677c88fe3bcd2844da4e78d2b754c5b83c58185
arsen1c <vector2912@gmail.com>
2023-02-17 01:45:19 +0530

    fix: databse name

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import { DB_URL } from "../config";
+import { DB_URL, DB_NAME } from "../config";
 
 export default async function connect() {
     const dbUri: string = DB_URL as string;
+    const dbName: string = DB_NAME as string;
 
     try {
-        await mongoose.connect(dbUri);
+        await mongoose.connect(dbUri, { dbName });
         console.log("DB Connected");
-
     } catch (error: any) {
         console.error("Could not connect to db");
         process.exit(1);
